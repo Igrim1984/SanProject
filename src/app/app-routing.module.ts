4bb193abd9ac5d1a00b3c9d19bc332bb644b0930
Emnilda <sandra.czolczynska@gmail.com>
2021-05-15 22:26:57 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './components/home/home.component'
+import { WcagComponent} from './components/wcag/wcag.component'
+import { InfoComponent } from './components/info/info.component'
+import { ContactComponent } from './components/contact/contact.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'wcag', component: WcagComponent},
+  {path: 'info', component: InfoComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WcagComponent } from './components/wcag/wcag.component';
+import { InfoComponent } from './components/info/info.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    WcagComponent,
+    InfoComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
